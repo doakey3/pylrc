a877d91ba270736642ecc715aff54b8563af2e5c
doakey3 <github.dmodo@spamgourmet.com>
2017-04-14 11:47:56 +0000

    gitignore file added

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='pylrc',
     packages=['pylrc'],
-    version='0.0.1',
+    version='0.0.2',
     description='A library for parsing .lrc files',
     long_description=open('README.rst').read(),
     author='doakey3',
     author_email='pylrc.dmodo@spamgourmet.com',
     url='https://github.com/doakey3/pylrc',
-    download_url='https://github.com/doakey3/pylrc/tarball/0.0.1',
+    download_url='https://github.com/doakey3/pylrc/tarball/0.0.2',
     license='MIT',
 )
