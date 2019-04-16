ba296d441afef5490b94a1031d073ebecd68a811
doakey3 <doakey3@gmail.com>
2019-04-16 14:36:09 -0500

    add changes to the setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='pylrc',
     packages=['pylrc'],
-    version='0.0.5',
+    version='0.0.7',
     description='A library for parsing .lrc files',
     long_description=open('README.rst').read(),
     author='doakey3',
     author_email='pylrc.dmodo@spamgourmet.com',
     url='https://github.com/doakey3/pylrc',
-    download_url='https://github.com/doakey3/pylrc/releases/download/0.0.5/pylrc.tar.gz',
+    download_url='https://github.com/doakey3/pylrc/releases/download/0.0.7/pylrc.tar.gz',
     license='MIT',
 )
