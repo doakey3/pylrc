41b2ae861bccae432b6746383062e2a512bddae5
doakey3 <github.dmodo@spamgourmet.com>
2019-08-22 12:29:17 -0700

    version update

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='pylrc',
     packages=['pylrc'],
-    version='0.0.8',
+    version='0.1.0',
     description='A library for parsing .lrc files',
     long_description=open('README.rst').read(),
-    author='doakey3',
+    author='doakey3, SimonIT',
     author_email='pylrc.dmodo@spamgourmet.com',
     url='https://github.com/doakey3/pylrc',
-    download_url='https://github.com/doakey3/pylrc/releases/download/0.0.8/pylrc.tar.gz',
+    download_url='https://github.com/doakey3/pylrc/releases/download/0.1.0/pylrc.tar.gz',
     license='MIT',
 )
